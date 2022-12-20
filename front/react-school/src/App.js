8baa49deca7127b38d73395aa478634f9ee1b63d
Otmahova <54909351+Otmahova@users.noreply.github.com>
2022-12-20 22:14:25 +0300

    Update App.js

--- front/react-school/src/App.js
+++ front/react-school/src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, {
+    useState
+} from 'react';
 
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
 import Main from './components/Main/Main.js'
 import Login from './components/Login/Login.js'
 import useToken from './components/Login/useToken';
@@ -15,23 +21,59 @@ import Journal from "./components/Pages/Journal";
 
 
 function App() {
-  const { token, setToken } = useToken();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/classes' element={<Classes />} />
-        <Route path='/schedule' element={<Schedule />} />
-        <Route path='/subjects' element={<Subjects />} />
-        <Route path='/journal' element={<Journal />} />
-        <Route path='/admin' element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const {
+        token,
+        setToken
+    } = useToken();
+    if (!token) {
+        return <Login setToken = {
+            setToken
+        }
+        />
+    }
+    return ( <
+        BrowserRouter >
+        <
+        Routes >
+        <
+        Route path = '/'
+        element = {
+            < Main / >
+        }
+        /> <
+        Route path = '/users'
+        element = {
+            < Users / >
+        }
+        /> <
+        Route path = '/classes'
+        element = {
+            < Classes / >
+        }
+        /> <
+        Route path = '/schedule'
+        element = {
+            < Schedule / >
+        }
+        /> <
+        Route path = '/subjects'
+        element = {
+            < Subjects / >
+        }
+        /> <
+        Route path = '/journal'
+        element = {
+            < Journal / >
+        }
+        /> <
+        Route path = '/admin'
+        element = {
+            < Admin / >
+        }
+        /> <
+        /Routes> <
+        /BrowserRouter>
+    );
 }
 
 export default App;
